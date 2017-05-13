73748447f3abf93a03e1b9d9abf3f6b810bf5a61
Serhii Kondratiuk <vielon.indie@yandex.ru>
2017-05-13 17:17:35 +0300

    reactcountdown-0.4-added-class-clock-logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import './App.css'
+import Clock from './Clock';
+import './App.css';
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -18,12 +20,8 @@ class App extends Component {
                 <div className="app-title">
                     Countdown to {this.state.deadline}
                 </div>
-                <div>
-                    <div className="clock-days">14 days</div>
-                    <div className="clock-hours">30 hours</div>
-                    <div className="clock-minutes">15 minutes</div>
-                    <div className="clock-seconds">20 seconds</div>
-                </div>
+                <Clock
+                 deadline={this.state.deadline}/>
                 <div>
                     <input
                         placeholder="new date"
